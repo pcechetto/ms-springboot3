4649718af972515c82cf33cd019f269ef6b10c38
pcechetto <pedrocechetto@hotmail.com>
2024-05-13 22:54:03 -0300

    Load Balancer

--- hr-worker/src/main/java/com/example/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/example/hrworker/resources/WorkerResource.java
@@ -3,7 +3,10 @@ package com.example.hrworker.resources;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +20,11 @@ import com.example.hrworker.repositories.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerRepository repository;
 
@@ -28,6 +36,9 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Optional<Worker>> findById(@PathVariable Long id) {
+
+        logger.info("PORT= " + env.getProperty("local.server.port"));
+
         Optional<Worker> obj = repository.findById(id);
         return ResponseEntity.ok().body(obj);
     }
