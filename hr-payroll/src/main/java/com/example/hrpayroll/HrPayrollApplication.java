f423098e1efd045162adfddbff1ce7edf7ff13c9
pcechetto <pedrocechetto@hotmail.com>
2024-05-13 23:23:57 -0300

    configuring eureka clients

--- hr-payroll/src/main/java/com/example/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/HrPayrollApplication.java
@@ -2,10 +2,10 @@ package com.example.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@LoadBalancerClient(name = "hr-worker")
+@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
