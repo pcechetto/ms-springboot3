9224371458b94d188d881b231d1f9ac278df60ba
pcechetto <pedrocechetto@hotmail.com>
2024-05-17 17:10:18 -0300

    Private git repository

--- hr-config-server/src/main/java/com/example/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/example/hrconfigserver/HrConfigServerApplication.java
@@ -7,9 +7,18 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class HrConfigServerApplication {
+	
+	// implements CommandLineRunner
+	// @Value("${spring.cloud.config.server.git.username}")
+	// private String username;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
+
+	// @Override
+	// public void run(String... args) throws Exception {
+	// 	System.out.println("Username = " + username);
+	// }
 
 }
