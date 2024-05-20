7384176786f40ac11b1b70a35b3ef6e320e8f391
pcechetto <pedrocechetto@hotmail.com>
2024-05-20 12:37:52 -0300

    database seeding

--- hr-user/src/main/java/com/example/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/example/hruser/HrUserApplication.java
@@ -6,10 +6,19 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication  {
+
+	// implements CommandLineRunner
+	// @Autowired
+	// private BCryptPasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
+
+	// @Override
+	// public void run(String... args) throws Exception {
+	// 	System.out.println("BCRYPT= " + passwordEncoder.encode("123456"));
+	// }
 
 }
