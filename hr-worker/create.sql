439a033d56073c4bad912bfad800ac791bf31222
pcechetto <pedrocechetto@hotmail.com>
2024-05-24 10:47:37 -0300

    hr-worker postgres dev profile

---
+++ hr-worker/create.sql
@@ -0,0 +1,4 @@
+create table tb_worker (daily_income float(53), id bigserial not null, name varchar(255), primary key (id));
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Bob', 200.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Maria', 300.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Alex', 250.0);
